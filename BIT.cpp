7f472b763f11bfdaeec6689462206d31a5ebec73
Ahmed Osama <76051700+ahmedosamaft@users.noreply.github.com>
2023-07-18 00:45:05 +0300

    Update BIT.cpp

--- BIT.cpp
+++ BIT.cpp
@@ -3,34 +3,12 @@
 ****************************************************/
 #include <bits/stdc++.h>
 using namespace std;
-#define ZA7med ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 typedef vector<int> vi;
-typedef vector<char> vc;
 using ll = long long;
 typedef vector<ll> vll;
-typedef pair<int, int> pii;
-typedef vector<pii> vpii;
-typedef map<int, int> mii;
-typedef set<int> si;
-#define pb(x) push_back(x)
 #define LSOne(x) ((x) & (-x))
-#define ss second
-#define ff first
 #define endl "\n"
-#define all(v) v.begin(), v.end()
-#ifndef ONLINE_JUDGE
-#include "debug.h"
-#else
-#define debug(x)
-void FRead() {}
-#endif
-// ==============================================================================================
-const ll N = 1e5 + 5, OO = 1000000000, mod = 1e9 + 7, LOO = 1e18;
-enum { UNVISITED = -1, VISITED = 1, EXPLORED = 2 };
-const int dx8[8] = {0, 0, 1, -1, 1, -1, -1, 1};
-const int dy8[8] = {1, -1, 0, 0, 1, -1, 1, -1};
-const int dx[4] = {0, 0, 1, -1};
-const int dy[4] = {1, -1, 0, 0};
+
 struct BIT {
 private:
   vll v;
@@ -64,22 +42,12 @@ public:
   ll rsq(int i, int j) { return rsq(j) - rsq(i - 1); }
 };
 
-void solve() {
+
+signed main() {
   vi v{0, 1, 2, 3, 4, 5, 6};
   BIT ft(v);
   cout << ft.rsq(3, 6) << endl;
   ft.update(3, 6);
   cout << ft.rsq(3, 6);
-}
-
-signed main() {
-  //  =============================================================================
-  ZA7med;
-  FRead();
-  //  =============================================================================
-  int TC = 1;
-  // cin >> TC;
-  while (TC--)
-    solve();
   return 0;
 }
